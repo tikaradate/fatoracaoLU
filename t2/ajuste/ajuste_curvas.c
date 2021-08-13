cb6e6b1acd600037c8fbaa39389124ce0c2c5fc5
tikaradate <tikaradate@gmail.com>
2021-08-12 22:24:24 -0300

    desenrolei e jammei o ajuste

--- t2/ajuste/ajuste_curvas.c
+++ t2/ajuste/ajuste_curvas.c
@@ -5,7 +5,7 @@
 #include "matriz.h"
 #include "gauss.h"
 
-struct matriz *montaAjuste(struct matriz *x)
+struct matriz *montaAjuste_original(struct matriz *x)
 {
     struct matriz *kurwa;
     int n = x->n;
@@ -41,6 +41,68 @@ struct matriz *montaAjuste(struct matriz *x)
 
     return kurwa;
 }
+
+struct matriz *montaAjuste(struct matriz *x)
+{
+    struct matriz *kurwa;
+    int i;
+    int n = x->n;
+    double *somas = calloc(n + n, sizeof(double));
+    double *pot = calloc(n * (n + n), sizeof(double));
+
+    for (int i = 0; i < n; i++)
+    {
+        pot[i] = 1;
+        somas[0] += 1;
+    }
+
+    int fator = 8;
+    for (i = 1; (i+fator) < (n+n)-((n+n)%fator); i+=fator)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            pot[i * n + j]       = pot[(i - 1) * n + j] * x->mat[j];
+            pot[(i + 1) * n + j] = pot[(i + 0) * n + j] * x->mat[j];
+            pot[(i + 2) * n + j] = pot[(i + 1) * n + j] * x->mat[j];
+            pot[(i + 3) * n + j] = pot[(i + 2) * n + j] * x->mat[j];
+            pot[(i + 4) * n + j] = pot[(i + 3) * n + j] * x->mat[j];
+            pot[(i + 5) * n + j] = pot[(i + 4) * n + j] * x->mat[j];
+            pot[(i + 6) * n + j] = pot[(i + 5) * n + j] * x->mat[j];
+            pot[(i + 7) * n + j] = pot[(i + 6) * n + j] * x->mat[j];
+            somas[i]       += pot[(i + 0) * n + j];
+            somas[(i + 1)] += pot[(i + 1) * n + j];
+            somas[(i + 2)] += pot[(i + 2) * n + j];
+            somas[(i + 3)] += pot[(i + 3) * n + j];
+            somas[(i + 4)] += pot[(i + 4) * n + j];
+            somas[(i + 5)] += pot[(i + 5) * n + j];
+            somas[(i + 6)] += pot[(i + 6) * n + j];
+            somas[(i + 7)] += pot[(i + 7) * n + j];
+        }
+    }
+    for (i = i; i < n+n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            pot[i * n + j] = pot[(i - 1) * n + j] * x->mat[j];
+            somas[i] += pot[(i + 0) * n + j];
+        }
+    }
+    
+    // preenche a matriz com os somatórios realizados acima
+    kurwa = alocaMatriz(n, n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            kurwa->mat[i * n + j] = somas[i + j];
+        }
+    }
+
+    free(somas);
+
+    return kurwa;
+}
+
 
 int ajusta(struct matriz *pontos, struct matriz *funcoes, struct matriz *somatorios, int i)
 {
