95b2e08286e842b587c8354491f393e4b05678b8
tikaradate <tikaradate@gmail.com>
2021-08-13 16:38:27 -0300

    organizando

--- t2/ajuste/jorge/ajuste_curvas.h
+++
@@ -1,8 +0,0 @@
-#ifndef __AJUSTE_CURVAS__
-#define __AJUSTE_CURVAS__
-
-struct matriz *montaAjuste(struct matriz *x);
-
-int ajusta(struct matriz *pontos, struct matriz *funcoes, struct matriz *somatorios, int i);
-
-#endif
--- t2/ajuste/jorge/interpolacao.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INTERPOLACAO__
-#define __INTERPOLACAO__
-
-struct matriz *montaInterpolacao(struct matriz *x);
-
-int interpola(struct matriz *pontos, struct matriz *funcoes, int i);
-
-#endif
